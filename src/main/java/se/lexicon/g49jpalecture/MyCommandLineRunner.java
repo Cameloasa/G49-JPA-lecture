b4d7603b15ca5b834003959e69c1f48bf033da7f
Camelia Ciuca <camelia1414@gmail.com>
2024-05-28 16:15:51 +0200

    MyCommandLineRunner

---
+++ src/main/java/se/lexicon/g49jpalecture/MyCommandLineRunner.java
@@ -0,0 +1,19 @@
+package se.lexicon.g49jpalecture;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import se.lexicon.g49jpalecture.entity.Student;
+import se.lexicon.g49jpalecture.repository.StudentRepository;
+
+@Component
+public class MyCommandLineRunner implements CommandLineRunner {
+
+    @Autowired
+    StudentRepository studentRepository;
+    @Override
+    public void run(String... args) throws Exception {
+
+            studentRepository.save(new Student("Test","Tetsson","test@test.com"));
+    }
+}
